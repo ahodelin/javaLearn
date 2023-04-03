b4e186540ac73a2df2bf9d88041ca979fd0c2dae
Abel Hodelín Hernández <abel.hodelin.hernandez@gmail.com>
2023-04-03 19:01:58 +0200

    Last update Mo 3. Apr 19:01:58 CEST 2023

--- healtrecords/HealthRecordsTest.java
+++ healtrecords/HealthRecordsTest.java
@@ -3,26 +3,38 @@ import java.util.Scanner;
 public class HealthRecordsTest {
 
 	public static void main(String[] args) {
-	Scanner input = new Scanner(System.in);
+		Scanner input = new Scanner(System.in);
         
-        System.out.print("Firstname: ");
-        String firstname = input.nextLine();
+        	System.out.print("Firstname: ");
+	        String firstname = input.nextLine();
 
-        System.out.print("Lastname: ");
-        String lastname = input.nextLine();
+        	System.out.print("Lastname: ");
+	        String lastname = input.nextLine();
    	
-        System.out.print("Year of birth: ");
-        short year = input.nextShort();
+		System.out.print("Gender: ");
+                String gender = input.nextLine();
 
-        System.out.print("Month of birth: ");
-        byte month = input.nextByte();
+        	System.out.print("Year of birth: ");
+        	short year = input.nextShort();
 
-        System.out.print("Day of birth: ");
-        byte day = input.nextByte();
+        	System.out.print("Month of birth: ");
+        	byte month = input.nextByte();
 
-        HealthRecords hr = new HealthRecords(firstname, lastname, year, month, day, "man", 1.76, 65.4);
-		
-	System.out.println(hr);
+       		System.out.print("Day of birth: ");
+        	byte day = input.nextByte();
+
+		System.out.print("Height: ");
+        	double height = input.nextDouble();
+
+		System.out.print("Weight: ");
+        	double weight = input.nextDouble();
+
+		//if((gender == "man" || gender == "woman") && firstname != "" && lastname != "" && year > 0 && month > 0 && height > 0 && weight > 0){
+			HealthRecords hr = new HealthRecords(firstname, lastname, year, month, day, gender, height, weight);
+			System.out.println(hr);
+		//}
+		//else
+		//	System.out.println("Input error.");
 
 	}
 
