3097586e340325b5e1df99d3cab0ad693de1d284
Abel Hodelin Hernandez <abel.hodelin.hernandez@gmail.com>
2023-04-21 10:20:16 +0200

    Last update Fr 21. Apr 10:20:16 CEST 2023

---
+++ computer_assisted_instruction/Multiplication.java
@@ -0,0 +1,16 @@
+// Multiplication for children
+
+public class Multiplication{
+  private static final SecureRandom randomNumbers = new SecureRandom();
+
+  public static void multiplication (){
+    byte number1 = (byte)(1 + randomNumbers.nextInt(10));
+    byte number1 = (byte)(1 + randomNumbers.nextInt(10));
+
+    
+    System.out.print("How much is " + number1 + " times " + number2);
+    
+    if
+    
+  }
+}
