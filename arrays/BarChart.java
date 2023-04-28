29a45039ab1905a17ea8a0ae7c1703f4a646ee50
Abel Hodelin Hernandez <abel.hodelin.hernandez@gmail.com>
2023-04-28 11:30:59 +0200

    Last update Fr 28. Apr 11:30:59 CEST 2023

--- arrays/BarChart.java
+++ arrays/BarChart.java
@@ -11,33 +11,9 @@ public class BarChart{
     for(int student = 0; student < students.length; student ++){
       int note = 1 + randomNumber.nextInt(100);
       students[student] = note;
-
-       if(note < 10)
-         grade[0] ++;
-       else if(note >= 10 && note < 20)
-         grade[1] ++;
-       else if(note >= 20 && note < 30)
-         grade[2] ++;
-       else if(note >= 30 && note < 40)
-         grade[3] ++;
-       else if(note >= 40 && note < 50)
-         grade[4] ++;
-       else if(note >= 50 && note < 60)
-         grade[5] ++;
-       else if(note >= 60 && note < 70)
-         grade[6] ++;
-       else if(note >= 70 && note < 80)
-         grade[7] ++;
-       else if(note >= 80 && note < 90)
-         grade[8] ++;
-       else if(note >= 90 && note < 100)
-         grade[9] ++;
-       else 
-         grade[10] ++;
+      grade[note / 10] ++;
     }
        
-//    int[] array = {};
-
     System.out.println("Grade distribution:");
 
     for(int counter = 0; counter < grade.length; counter ++){
