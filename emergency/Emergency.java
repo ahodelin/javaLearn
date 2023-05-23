fbc431a8f7932120a9ea1d422766fa30c1a430f6
Abel Hodelin Hernandez <abel.hodelin.hernandez@gmail.com>
2023-05-23 09:55:42 +0200

    Last update Di 23. Mai 09:55:42 CEST 2023

--- emergency/Emergency.java
+++ emergency/Emergency.java
@@ -3,13 +3,24 @@ public class Emergency{
   private Person person;
   private Address address;
   private Location location;
+  private Time time;
+//  private
 
-  private
-
-  public Emergency (Person p, Address a, Location l){
+  public Emergency (Person p, Address a, Location l, Time t){
     p = new Person("Pepito", "El Loco", "443025");
     this.person = p;
 
-    a = new Addres("Test City", "La calle del medio", "55331", "")
-  } 
+    a = new Address("Test City", "La calle del medio", "55331", "Edificio Foxa", "4");
+    this.address = a;
+
+    l = new Location(24.2, 54.6);
+    this.location = l;
+
+    t = new Time(23, 44, 53);
+    this.time = t;
+  }
+
+   public String toString(){
+     return "" + this.person + this.address + this.location + this.time;
+   }
 }
---
+++ emergency/EmergencyTest.java
@@ -0,0 +1,5 @@
+public class EmergencyTest{
+  public static void main(String[] args){
+    
+  }
+}
