e6579fdf97b35887bbeb9eaaa54d47d3185118a4
Abel Hodelin Hernandez <abel.hodelin.hernandez@gmail.com>
2023-06-12 16:27:27 +0200

    Last update Mo 12. Jun 16:27:27 CEST 2023

---
+++ polymorphismInterfaces/SalariedEmployeeTest.java
@@ -0,0 +1,7 @@
+public class SalariedEmployeeTest{
+  public static void main(String[] args){
+    SalariedEmployee se = new SalariedEmployee("Abel", "Hodelín Hernández", "111-11-1111", 4900);
+
+    System.out.println(se);
+  }
+}
