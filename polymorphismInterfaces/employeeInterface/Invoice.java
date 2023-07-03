2b3123be3a80d1c7928ccbadfe8f3adac1ac1304
Abel Hodelin Hernandez <abel.hodelin.hernandez@gmail.com>
2023-07-03 11:29:00 +0200

    Last update Mo 3. Jul 11:29:00 CEST 2023

--- polymorphismInterfaces/employeeInterface/Invoice.java
+++ polymorphismInterfaces/employeeInterface/Invoice.java
@@ -17,5 +17,39 @@ public class Invoice implements Payable{
     this.pricePerItem = pricePerItem;
   }
 
- // public String getPartNumber(){} 
+  public String getPartNumber(){
+    return this.partNumber;
+  }
+
+  public String getPartDescription(){
+    return this.partDescription;
+  }
+
+  public void setQuantity(int quantity){
+    if(quantity < 0)
+      throw new IllegalArgumentException("Quantity mus be >= 0");
+
+    this.quantity = quantity;
+  }
+
+  public int getQuantity(){
+    return this.quantity;
+  }
+
+  public double getPricePerItem(){
+    return this.pricePerItem;
+  }
+
+  @Override
+  public String toString(){
+    return String.format("%s: %n%s: %s (%s) %n%s: %d %n%s: $%,.2f",
+      "invoice", "part number", this.getPartNumber(), this.getPartDescription(),
+      "quantity", this.getQuantity(), "price per item", this.getPricePerItem()
+    );
+  }
+
+  @Override
+  public double getPaymentAmount(){
+    return this.getQuantity() * this.getPricePerItem();
+  }
 }
