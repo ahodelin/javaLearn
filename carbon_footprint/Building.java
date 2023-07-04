30c386880b05b130703954ea3c453d4b5c1f8744
Abel Hodelin Hernandez <abel.hodelin.hernandez@gmail.com>
2023-07-04 14:12:03 +0200

    Last update Di 4. Jul 14:12:03 CEST 2023

---
+++ carbon_footprint/Building.java
@@ -0,0 +1,7 @@
+public class Building implements CarbonFootprint{
+  private final String heatingTechnology;
+  private final double electricity;
+  private boolean isGreenEnergy;
+
+  
+}
---
+++ carbon_footprint/Car.java
@@ -0,0 +1,27 @@
+public class Car implements CarbonFootprint{
+  private static final double Factor = 2.370;
+  private double consumptionPer100Km;
+
+  public Car(double consumpitonPer100Km){
+    if(consumpitonPer100Km < 0)
+      throw new IllegalArgumentException("The comsumption per 100 km must be >= 0");
+
+    this.consumptionPer100Km = consumptionPer100Km;
+  }
+
+  public double getConsumptionPer100Km(){
+    return this.consumptionPer100Km;
+  }
+
+  @Override
+  public double getCarbonFootprint(){
+    return Factor * this.getConsumptionPer100Km();
+  }
+
+  @Override
+  public String toString(){
+    return String.format("%s%n%s%.2f",
+      "Car", "Carbon Footprint: ", this.getCarbonFootprint()
+    );
+  }
+}
