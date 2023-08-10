147c31be7f4a28327023519c2bb31b259d6bf57f
Abel Hodelin Hernandez <abel.hodelin.hernandez@gmail.com>
2023-08-10 11:53:21 +0200

    Last update Do 10. Aug 11:53:21 CEST 2023

---
+++ gui/MouseTrackerFrame.java
@@ -0,0 +1,72 @@
+import java.awt.Color;
+import java.awt.BorderLayout;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+import java.awt.event.MouseEvent;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class MouseTrackerFrame extends JFrame{
+  private final JPanel mousePanel;
+  private final JLabel statusBar;
+
+  public MouseTrackerFrame(){
+    super("Demonstrating Mouse Events");
+
+    mousePanel = new JPanel();
+    mousePanel.setBackground(Color.WHITE);
+    add(mousePanel, BorderLayout.CENTER);
+
+    statusBar = JLabel("Mouse outside JPanel");
+    add(statusBar, BorderLayout.SOUTH);
+
+    MouseHandler handler = new MouseHandler();
+    MouseHandler.addMouseListener(handler);
+    MouseHandler.addMouseMotionListener(handler);
+  }
+
+  private class MouseHandler implements MouseListener,
+    MouseMotionListener{
+
+    @Override
+    public void mouseClicked(MouseEvent event) {// aqui pueder ir un apostrofe "'"
+      statusBar.setText(String.format("Clicked at [%d, %d]",
+        event.getX(), event.getY()
+      ));
+    }
+
+    @Override
+    public void mousePressed(MouseEvent event){
+      statusBar.setText(String.format("Pressed at [%d, %d]",
+        event.getX(), event.getY()
+      ));
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent event){
+      statusBar.setText(String.format("Released at [%d, %d]",
+        event.getX(), event.getY()
+      ));
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent event){
+      statusBar.setText(String.format("Mouse entered at [%d, %d]",
+        event.getX(), event.getY()
+      ));
+      mousePanel.setBackground(Color.GREEN);
+    }
+
+    @Override
+    public void mouseExited(MouseEvent event){
+      statusBar.setText("Mouse outside JPanel");
+      mousePanel.setBackground(Color.WHITE);
+    }
+
+    @Override
+    public void mouseDraged(MouseEvent event){
+      
+    }
+  }
+}
